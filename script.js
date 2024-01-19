be3b2e7659a15d92d60eb2811d52fbc3e3d04c96
iam-msr <sriram.mudragada4269+github@gmail.com>
2024-01-19 09:23:57 +0530

    added main funtions

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+
+
+function showUserLoader() {
+    document.getElementById("UserLoader").style.display = "block";
+  }
+  
+  function hideUserLoader() {
+    document.getElementById("UserLoader").style.display = "none";
+  }
+  
+  function showRepoLoader() {
+    document.getElementById("RepoLoader").style.display = "block";
+  }
+  
+  function hideRepoLoader() {
+    document.getElementById("RepoLoader").style.display = "none";
+  }
+
+
 function getInfo() 
 {
+    showUserLoader();
   const usernameInput = document.getElementById("username");
   const username = usernameInput.value.trim();
 
@@ -30,10 +50,13 @@ async function getUserdetails(username) {
         }
       }
   
-  
+        // Hide loader
+        hideUserLoader();
       const userData = await response.json();
-  
-  
+      
+      // Update totalRepos
+    totalRepos = userData.public_repos;
+ 
       // Display user details
       const userDetailsElement = document.getElementById('userDetails');
       userDetailsElement.innerHTML = `
@@ -66,3 +89,45 @@ async function getUserdetails(username) {
       }
     }
   }
+
+  async function getRepos(username) {
+    try {
+      // Show Repo loader
+      showRepoLoader();
+  
+      const apiUrlRepos = `https://api.github.com/users/${username}/repos?page=${currentPage}&per_page=${itemsPerPage}`;
+  
+      // Fetch repositories
+      const response = await fetch(apiUrlRepos);
+  
+      if (!response.ok) {
+        if (response.status === 404) {
+          throw { message: "User not found", statusCode: 404 };
+        } else {
+          throw { message: `HTTP error! Status: ${response.status}`, statusCode: response.status };
+        }
+      }
+  
+      // Hide loader
+      hideRepoLoader();
+  
+      const data = await response.json();
+  
+      displayRepos(data);
+    } catch (error) {
+      // Hide loader
+      hideRepoLoader();
+  
+      console.error("Error fetching repositories:\n", error);
+  
+      const paginationElement = document.getElementById("paginationPage");
+      paginationElement.style.display = "none";
+  
+      if (error.statusCode === 404) {
+        const repoListElement = document.getElementById("repoList");
+        repoListElement.innerHTML = `<p style="color:red; font-weight:bold;font-size:xx-large;text-align:center;margin:auto;margin-bottom:40px">No repositories found.</p>`;
+      }
+    }
+  }
+
+  function displayRepos(repos) {}
